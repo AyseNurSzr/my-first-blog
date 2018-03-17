371e6725151cf6739ac9f56fc5367f59031cff07
AyseNurSzr <ayse-nur.sezer.a@gmail.com>
2018-03-17 21:09:43 +0100

    Modification du HTML du site

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,6 @@
-from django.urls import path
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    path('accueil', views.home),
-    path('base', views.acc),
-    path('date', views.date_actuelle),
-    path('addition/<int:nombre1>/<int:nombre2>/',views.addition),
-    
+    url(r'^$', views.base,name='base'),
     ]
